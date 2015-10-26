b64c29400080c6adf5cc09611fee72f3462187c2
Adrian Eguez <eadepto@hotmail.com>
2015-10-26 14:05:56 -0500

    Cambiar carpeta task a Pokedex  Crear Modelo Pokemon

--- Pokedex/api/models/Pokemon.js
+++ Pokedex/api/models/Pokemon.js
@@ -1,14 +1,42 @@
 /**
-* Pokemon.js
-*
-* @description :: TODO: You might write a short summary of how this model works and what it represents here.
-* @docs        :: http://sailsjs.org/#!documentation/models
-*/
+ * Pokemon.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/#!documentation/models
+ */
 
 module.exports = {
 
-  attributes: {
+    attributes: {
 
-  }
+        nombre: {
+            type: 'string',
+            unique: true,
+            required: true
+        },
+        tipo: {
+            type: 'string',
+            enum: ['agua', 'fuego', 'psiquico', 'hierba', 'electrico', 'bicho']
+        },
+        habilidadEspecial: {
+            type: 'string',
+            defaultsTo: 'Ninguno'
+        },
+        numerPokemon: {
+            type: 'integer',
+            required:true
+        },
+        fechaVista:{
+            type:'date'
+        }
+
+    }
 };
+
+
+
+
+
+
+
 
